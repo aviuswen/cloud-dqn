29dfe47d9abd379131d0ac5a714333546c4fc0be
Dan Quoc Nguyen <aviuswen@gmail.com>
2017-11-02 21:44:40 -0700

    Lambda handling requests as input streams comments about utilized without Lambda Proxy Integration

--- src/main/kotlin/cloud/LambdaHandler.kt
+++ src/main/kotlin/cloud/LambdaHandler.kt
@@ -35,10 +35,50 @@ class LambdaHandler: RequestStreamHandler {
 
     /**
      * Utilizing AWS Lambda Proxy integrations (No aws integrated response after lambda finishes)
+     *  Request body (attribute: input) is:
+        {
+          "params": {
+            "resource": "/test",
+            "path": "/test",
+            "httpMethod": "POST",
+            "headers": null,
+            "queryStringParameters": null,
+            "pathParameters": null,
+            "stageVariables": null,
+            "requestContext": {
+              "path": "/test",
+              "accountId": "321763696197",
+              "resourceId": "porzx3",
+              "stage": "test-invoke-stage",
+              "requestId": "test-invoke-request",
+              "identity": {
+                "cognitoIdentityPoolId": null,
+                "accountId": "321763696197",
+                "cognitoIdentityId": null,
+                "caller": "321763696197",
+                "apiKey": "test-invoke-api-key",
+                "sourceIp": "test-invoke-source-ip",
+                "accessKey": "ASIAJOSTS2HBTNNW4QZA",
+                "cognitoAuthenticationType": null,
+                "cognitoAuthenticationProvider": null,
+                "userArn": "arn:aws:iam::321763696197:root",
+                "userAgent": "Apache-HttpClient/4.5.x (Java/1.8.0_144)",
+                "user": "321763696197"
+              },
+              "resourcePath": "/test",
+              "httpMethod": "POST",
+              "apiId": "08w9yyxz82"
+            },
+            "body": "THE REQUEST BODY AS A STRING",
+            "isBase64Encoded": false
+          }
+}
      *  Required to write out to output stream a json in string format with the following keys:
      *      body: as a String
      *      statusCode: Int
      *      headers: Json Object
+     *
+     * If not being used in Lambda Proxy Integration, attribute: input is request body only;
      */
     override fun handleRequest(input: InputStream, output: OutputStream, context: Context) {
         val jsonParser = JsonParser()
